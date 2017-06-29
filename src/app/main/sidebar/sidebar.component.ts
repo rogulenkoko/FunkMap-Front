23736b2441bafed9840433e882f7d0978a609ae3
K.rogulenko <rogulenkoko@gmail.com>
2017-06-30 00:36:55 +0300

    Выделение выбранного в боковой панели

--- src/app/main/sidebar/sidebar.component.ts
+++ src/app/main/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from "../user/user.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'sidebar',
@@ -8,14 +9,54 @@ import { UserService } from "../user/user.service";
 })
 export class SidebarComponent implements OnInit {
 
-  constructor(private userService: UserService) { }
+  @ViewChild("messageCountTemplate") messageCountTemplate;
 
-  ngOnInit() {
+  private topItems: Array<SidebarItem>;
+  private bottomItems: Array<SidebarItem>;
+
+  constructor(private userService: UserService,
+    private route: ActivatedRoute) {
+
+
   }
 
-  logOut(){
+  ngOnInit() {
+    this.topItems = [
+      new SidebarItem("search", "Search", "search-icon"),
+      new SidebarItem("", "Person", "person-icon"),
+      new SidebarItem("", "Messages", "messenger-icon", null, this.messageCountTemplate),
+      new SidebarItem("favorites", "Favorites", "star-icon")
+    ]
+
+    this.bottomItems = [
+      new SidebarItem("settings", "Settings", "settings-icon"),
+      new SidebarItem("", "Logout", "exit-icon", ()=> this.logOut())
+    ]
+  }
+
+  onItemClick(item: SidebarItem){
+    if(item.clickEvent){
+      item.clickEvent();
+    }
+
+    var allItems = this.bottomItems.concat(this.topItems);
+    allItems.forEach(item => {
+      item.isSelected = false;
+    });
+    item.isSelected = !item.isSelected;
+  }
+
+  logOut() {
     this.userService.user = undefined;
     this.userService.avatar = undefined;
   }
 
+}
+
+export class SidebarItem {
+  constructor(public route: string, public title, public iconClass, public clickEvent?:()=>void, public rightTemplate?: any) {
+
+  }
+
+  public isSelected: boolean;
 }
