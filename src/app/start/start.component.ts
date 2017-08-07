e722b2038e57e534cf1470301560d5501bfbe42a
K.rogulenko <rogulenkoko@gmail.com>
2017-08-08 01:46:14 +0300

    Верстка стартовой страницы

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MapProvider } from "app/main/map/map-provider.service";
+import { Map, EntityType } from "app/main/map/models";
 
 @Component({
   selector: 'start',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  private map: L.Map;
+  private baseLayer: L.TileLayer;
+
+  private items: Array<StartPageItem>;
+
+  constructor(private mapProvider: MapProvider) { }
 
   ngOnInit() {
+    this.map = new L.Map('map', { center: new L.LatLng(30, 10), zoom: 0, zoomAnimation: false, zoomControl: false });
+    this.baseLayer = new L.TileLayer(this.mapProvider.selectedMap.url, this.buildMapOptions(this.mapProvider.selectedMap));
+    this.map.addLayer(this.baseLayer);
+
+
+    this.items = [
+      new StartPageItem("icon-musician-start", "Start_Musician", EntityType.Musician),
+      new StartPageItem("icon-band-start", "Start_Band", EntityType.Band),
+       new StartPageItem("icon-studio-start", "Start_Studio", EntityType.Studio),
+      new StartPageItem("icon-shop-start", "Start_Shop", EntityType.Shop),
+      new StartPageItem("icon-rehearsal-start", "Start_Rehearsal", EntityType.RehearsalPoint)
+    ];
   }
 
+  private buildMapOptions(map: Map): any {
+    let options: any;
+    if (map.subdomains.length == 0) {
+      options = {
+        attribution: map.attribution,
+        maxZoom: map.maxZoom,
+        minZoom: 3
+      };
+    }
+    else {
+      options = {
+        attribution: map.attribution,
+        maxZoom: map.maxZoom,
+        subdomains: map.subdomains,
+        minZoom: 3
+      };
+    }
+    return options;
+  }
+
+}
+
+export class StartPageItem{
+  constructor(public iconClass: string, public title: string, public value: EntityType){
+
+  }
 }
