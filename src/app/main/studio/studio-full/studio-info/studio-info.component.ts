03ca44831d583e0c329e97f8f83a2bb56baedc81
K.rogulenko <rogulenkoko@gmail.com>
2017-08-22 20:36:48 +0300

    Инфо для студии

--- src/app/main/studio/studio-full/studio-info/studio-info.component.ts
+++ src/app/main/studio/studio-full/studio-info/studio-info.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Studio } from "app/main/studio/models/studio-preview";
+import { InfoItem } from "app/tools/entity-full/entity-info/entity-info.component";
+import { EditService } from "app/tools/entity-full/edit.service";
+import { TranslateService } from "@ngx-translate/core";
+import { StudioService } from "app/main/studio/studio.service";
 
 @Component({
   selector: 'studio-info',
@@ -7,9 +12,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudioInfoComponent implements OnInit {
 
-  constructor() { }
+  private studio: Studio;
+  private newStudio: Studio;
+
+
+  @ViewChild("descriptionEditTemplate") descriptionEditTemplate: any;
+  @ViewChild("webSiteEditTemplate") webSiteEditTemplate: any;
+  @ViewChild("webSiteValueTemplate") webSiteValueTemplate: any;
+
+  private infoItems: Array<InfoItem>;
+
+  private allTitle: string;
+
+
+  constructor(private editService: EditService,
+              private translateService: TranslateService,
+              private studioService: StudioService) {
+     this.translateService.get("Choose").subscribe(value=> this.allTitle = value);
+  }
 
   ngOnInit() {
+    this.studio = this.editService.baseModel as Studio;
+    this.updateInfoItems();
+  }
+
+  private updateInfoItems() {
+    this.newStudio = Object.create(this.studio);
+
+
+    var webSiteInfoItem = new InfoItem();
+    webSiteInfoItem.propertyTitle = "Website";
+    webSiteInfoItem.propertyTemplate = this.webSiteValueTemplate;
+    webSiteInfoItem.propertyEditTemplate = this.webSiteEditTemplate;
+    
+    var descriptionItem = new InfoItem();
+    descriptionItem.propertyTitle = "Musician_Description";
+    descriptionItem.propertyValue = this.studio.description;
+    descriptionItem.propertyEditTemplate = this.descriptionEditTemplate;
+
+    this.infoItems = [
+      webSiteInfoItem,
+      descriptionItem
+    ]
+  }
+
+  private refreshStudio(login: string) {
+    this.studioService.getStudio(login).subscribe(band => {
+      this.studio = band;
+      this.updateInfoItems();
+      this.editService.baseModel = band;
+      this.editService.onSaved.emit();
+    })
+  }
+
+  save() {
+    this.newStudio.login = this.studio.login;
+    this.studioService.updateStudio(this.newStudio).subscribe(x => {
+      this.refreshStudio(this.studio.login);
+    })
+  }
+
+  cancel() {
+    this.refreshStudio(this.studio.login);
+    this.newStudio = Object.create(this.studio);
   }
 
 }
