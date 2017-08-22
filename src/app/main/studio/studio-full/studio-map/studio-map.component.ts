811b05ead0e45bcd1076025a94073783afb8922f
K.rogulenko <rogulenkoko@gmail.com>
2017-08-22 19:00:24 +0300

    Карта для студий

--- src/app/main/studio/studio-full/studio-map/studio-map.component.ts
+++ src/app/main/studio/studio-full/studio-map/studio-map.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Studio } from "app/main/studio/models/studio-preview";
+import { EntityType, Marker } from "app/main/map/models";
+import { IconProvider } from "app/main/map/icon-provider.service";
+import { MapCreationService } from "app/main/map/map-creation.service";
+import { EntityMapComponent } from "app/tools/entity-full/entity-map/entity-map.component";
+import { EditService } from "app/tools/entity-full/edit.service";
+import { StudioService } from "app/main/studio/studio.service";
 
 @Component({
   selector: 'studio-map',
@@ -7,9 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudioMapComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('entityMap') entityMap: EntityMapComponent;
+
+  private studio: Studio;
+  private marker: Marker;
+
+  constructor(private mapCreationService: MapCreationService,
+              private iconProvider: IconProvider,
+              private studioService: StudioService,
+              private editService: EditService) { 
+    this.studio = this.editService.baseModel as Studio;
+  }
 
   ngOnInit() {
+    this.marker = this.buildMarker();
+  }
+
+  private buildMarker():Marker{
+    var marker = new Marker(this.studio.login, this.studio.latitude, this.studio.longitude, EntityType.Studio);
+    marker.iconUrl = this.iconProvider.getIcon(marker);
+    return marker;
+  }
+
+  private onBaseSaved(){
+    var band = new Studio();
+    band.login = this.editService.baseModel.login;
+    band.latitude = this.editService.baseModel.latitude;
+    band.longitude = this.editService.baseModel.longitude;
+    this.studioService.updateStudio(band).subscribe(response => {
+      
+    });
   }
 
 }
