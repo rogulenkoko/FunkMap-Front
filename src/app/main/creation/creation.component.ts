da6615d569d33a0e1836414f2e3d9a3c67346d01
K.rogulenko <rogulenkoko@gmail.com>
2017-08-23 09:56:19 +0300

    Комит для деплоя 3

--- src/app/main/creation/creation.component.ts
+++ src/app/main/creation/creation.component.ts
@@ -64,7 +64,7 @@ export class CreationComponent implements OnInit {
         var route = RouteBuilder.buildRoute(this.creationService.selectedEntity, this.creationService.baseModel.login);
         this.router.navigate([route]);
       }
-    });
+    }); 
 
   }
 
