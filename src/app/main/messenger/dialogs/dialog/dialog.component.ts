f6a01dbf8c192125def84488e25eb144face6ece
K.rogulenko <rogulenkoko@gmail.com>
2017-08-25 17:54:41 +0300

    Аватрки диалогов

--- src/app/main/messenger/dialogs/dialog/dialog.component.ts
+++ src/app/main/messenger/dialogs/dialog/dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dialog } from "app/main/messenger/models";
 import { DialogService } from "app/main/messenger/dialog.service";
+import { UserService } from "app/main/user/user.service";
+import { UserDataService } from "app/main/user/user-data.service";
 
 @Component({
   selector: 'dialog-item',
@@ -11,11 +13,18 @@ export class DialogComponent implements OnInit {
 
   @Input() dialog: Dialog;
 
-  constructor(private dialogService: DialogService) {
+  constructor(private dialogService: DialogService,
+              private userService: UserService,
+              private userDataService: UserDataService) {
    }
 
   ngOnInit() {
-    //console.log(this.dialog);
+    if(this.userService.user.login && this.dialog.participants && this.dialog.participants.length == 2){
+      var login = this.dialog.participants.filter(x=>x != this.userService.user.login)[0];
+      this.userDataService.getImage(login).subscribe(image=>{
+        this.dialog.avatar = image;
+      })
+    }
   }
 
 }
