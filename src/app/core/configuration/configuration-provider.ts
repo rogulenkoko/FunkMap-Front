3aa0c6d8a9fecbf7913e59971ff0a233d0df01bc
K.rogulenko <rogulenkoko@gmail.com>
2017-08-29 18:13:51 +0300

    Конфигурация под удаленный сервер

--- src/app/core/configuration/configuration-provider.ts
+++ src/app/core/configuration/configuration-provider.ts
@@ -2,7 +2,7 @@ import { environment } from "environments/environment";
 export class ConfigurationProvider{
 
     public static get apiUrl(): string{
-        return environment.local ? "http://localhost:9000/api/" :  "http://136.243.24.102:33967/api/"; 
+        return environment.local ? "http://localhost:9000/api/" :  "http://95.213.239.58:14080/api/"; 
     }
 
 }
