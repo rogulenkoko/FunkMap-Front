08e2da1c8b817b5a80f82e503002d1146cbd9ad4
K.rogulenko <rogulenkoko@gmail.com>
2017-09-05 02:19:52 +0300

    Обработка ошибок авторизованных запросов

--- src/app/core/http/http-client.service.ts
+++ src/app/core/http/http-client.service.ts
@@ -6,39 +6,41 @@ import { AuthResponse } from "app/main/login/login-response";
 import { Observable } from "rxjs/Observable";
 import { ConfigurationProvider, ServiceType } from "app/core/configuration/configuration-provider";
 import { Scheduler } from "rxjs/Scheduler";
+import 'rxjs/add/operator/catch';
+import { Router } from "@angular/router";
 
 @Injectable()
 export class HttpClient {
 
   private options: RequestOptions;
 
-  constructor(private http: Http, private userService: UserService) {
+  constructor(private http: Http, private userService: UserService, private router: Router) {
     this.initOptions();
-    this.updateOptions(); 
+    this.updateOptions();
     this.userService.onUserChanged.subscribe(() => this.updateOptions());
   }
 
   public post(url: string, data: any, options?: any): Observable<any> {
-    if(this.userService.user && this.userService.user.authData && new Date(this.userService.user.authData.expiresDate) <= new Date()){
-      return this.prolongate().switchMap(response=>{
-        
+    if (this.userService.user && this.userService.user.authData && new Date(this.userService.user.authData.expiresDate) <= new Date()) {
+      return this.prolongate().switchMap(response => {
+
         this.setRefreshedData(response, true);
         this.updateOptions();
-        return this.http.post(url, data, options ? options : this.options); 
-      })
+        return this.http.post(url, data, options ? options : this.options).catch(error=> this.handleError(error));
+      }).catch(error => this.handleError(error))
     }
-    return this.http.post(url, data, options ? options : this.options);
+    return this.http.post(url, data, options ? options : this.options).catch(error=> this.handleError(error));
   }
 
   public get(url: string, options?: any): Observable<any> {
-    if(this.userService.user && this.userService.user.authData && new Date(this.userService.user.authData.expiresDate) <= new Date()){
-      return this.prolongate().switchMap(response=>{
+    if (this.userService.user && this.userService.user.authData && new Date(this.userService.user.authData.expiresDate) <= new Date()) {
+      return this.prolongate().switchMap(response => {
         this.setRefreshedData(response, true);
         this.updateOptions();
-        return this.http.get(url, options ? options : this.options); 
-      })
+        return this.http.get(url, options ? options : this.options).catch(error=> this.handleError(error));
+      }).catch(error => this.handleError(error))
     }
-    return this.http.get(url, options ? options : this.options);
+    return this.http.get(url, options ? options : this.options).catch(error=> this.handleError(error));
   }
 
   //todo подумать может стоит вынести и в каком месте использовать
@@ -54,21 +56,30 @@ export class HttpClient {
     params.set("refresh_token", this.userService.user.authData.refreshToken);
     params.set("grant_type", "refresh_token");
 
-    return this.http.post(`${ConfigurationProvider.apiUrl(ServiceType.Auth)}token`, params, options).map(x => AuthResponse.ToLoginResponsne(x.json()));//.subscribe((response)=>)
+    return this.http.post(`${ConfigurationProvider.apiUrl(ServiceType.Auth)}token`, params, options).map(x => AuthResponse.ToLoginResponsne(x.json()));
   }
 
-  private setRefreshedData(authData: AuthResponse, withoutUserUpdate?: boolean){
-    if(withoutUserUpdate){
+  private handleError(error: any): Observable<any> {
+    console.log(error);
+    if(error.status == 401){
+      this.userService.user = undefined;
+      this.router.navigate(['/login']);
+    }
+    return Observable.of(error);
+  }
+
+  private setRefreshedData(authData: AuthResponse, withoutUserUpdate?: boolean) {
+    if (withoutUserUpdate) {
       this.userService.user.authData = authData;
       return;
     }
     var user = new User();
     user.login = authData.login;
     user.authData = authData;
-    this.userService.user = user; 
+    this.userService.user = user;
   }
 
-  private initOptions(){
+  private initOptions() {
     this.options = new RequestOptions();
     var headers = new Headers();
     headers.append("Content-Type", "application/json");
