f481a9aeff58b4ae2c9773e90e8a3291ee3c95af
K.rogulenko <rogulenkoko@gmail.com>
2017-10-07 01:42:49 +0300

    Правильное местоположение маркеров

--- src/app/main/map/marker-factory.service.ts
+++ src/app/main/map/marker-factory.service.ts
@@ -17,8 +17,10 @@ export class MarkerFactory {
       login: point.login,
       icon: new L.DivIcon({
         html:`
-          <div class="icon-image-container d-flex align-items-center justify-content-center">
-            <img src="${point.iconUrl}"/>
+          <div class="main-icon-image-container">
+            <div class="icon-image-container d-flex align-items-center justify-content-center">
+              <img src="${point.iconUrl}"/>
+            </div>
           </div>
         `
       })
@@ -40,13 +42,15 @@ export class MarkerFactory {
     var marker = L.marker(new L.LatLng(point.lat, point.lng),{
       icon: new L.DivIcon({
         html:`
-        <div class="creation-icons-container d-flex flex-row">
-        <div id="cross" class="cross-container d-flex align-items-center justify-content-center"><img id="cross-img" src="assets/images/close-red.png"/></div>
-        <div id="apply" class="apply-container d-flex align-items-center justify-content-center"><img id="apply-img" src="assets/images/apply.png"/></div>
-      </div>
-          <div class="creation-marker-container d-flex align-items-center justify-content-center">
-            
-            <img src="${point.iconUrl}"/>
+        <div class="main-creation-marker-container">
+            <div class="creation-icons-container d-flex flex-row">
+              <div id="cross" class="cross-container d-flex align-items-center justify-content-center"><img id="cross-img" src="assets/images/close-red.png"/></div>
+              <div id="apply" class="apply-container d-flex align-items-center justify-content-center"><img id="apply-img" src="assets/images/apply.png"/></div>
+            </div>
+            <div class="creation-marker-container d-flex align-items-center justify-content-center">
+              
+              <img src="${point.iconUrl}"/>
+            </div>
           </div>
         `
       })
