23139fd5bd5d10d4298c5e69f64f2b453f612841
K.rogulenko <rogulenkoko@gmail.com>
2017-12-15 02:40:18 +0300

    Сигналр в месенджере работает корректно

--- src/app/tools/signalr/signalr.service.ts
+++ src/app/tools/signalr/signalr.service.ts
@@ -24,18 +24,17 @@ export abstract class SignalrService {
   }
 
   protected checkConnectionStatus(connection: ISignalRConnection, config: SignalRConfiguration): Observable<ISignalRConnection> {
+    
     if (!connection) return this.createHubConnection(config);
     var jConnection = (<any>connection)._jConnection;
-    return Observable.of(this._connection);
-    // console.log(jConnection.state);
-    // switch (jConnection.state) {
-    //   //{connecting: 0, connected: 1, reconnecting: 2, disconnected: 4}
-    //   case 1: return Observable.of(connection);
-    //   case 0: return Observable.of(connection);//todo
-    //   case 4: return this.createHubConnection(config); //disconnected
-    //   case 2: return Observable.of(null);
-    //   default: return Observable.of(null);
-    // }
+    switch (jConnection.state) {
+     //{connecting: 0, connected: 1, reconnecting: 2, disconnected: 4}
+      case 1: return Observable.of(connection);
+      case 0: return Observable.of(connection);//todo
+      case 4: return this.createHubConnection(config); //disconnected
+      case 2: return Observable.of(null);
+      default: return Observable.of(null);
+    }
   }
 
   abstract createHubConnection(config: SignalRConfiguration): Observable<ISignalRConnection>;
@@ -56,45 +55,29 @@ export class SignalrServiceReal extends SignalrService {
     connectionOptions.qs = {};
     connectionOptions.qs['login'] = this.userService.user ? this.userService.user.login : "";
     connectionOptions.url = ConfigurationProvider.apiUrl(ServiceType.Messenger).replace("/api/", "");
+
+
     return connectionOptions;
   }
 
   constructor(private signalR: SignalR,
     private userService: UserService) {
     super();
-    this.userService.onUserChanged.subscribe(() =>  this.createHubConnection(this.configuration));
+    this.userService.onUserChanged.subscribe(() =>{
+      return this.createHubConnection(this.configuration);
+    });
   }
 
 
   public createHubConnection(config: SignalRConfiguration): Observable<ISignalRConnection> {
-
-    if(!this.userService.user){
-      console.log("дисконнект");
-      if(this._connection) this._connection.stop();
-      return Observable.of(null);
-    }
-
     if(this._connection){
-
-      this._connection.status.subscribe(s=>console.log("stat",s));
-
-      console.log("дисконнект");
-      console.log((<any>this._connection)._jConnection.state);
-      var state = (<any>this._connection)._jConnection.state;
       this._connection.stop();
+      this._connection = undefined;
     }
-
-   
-
-    return Observable.fromPromise(this.signalR.connect(config)).switchMap(connection => {
-      this._connection = connection;
-      this._connection.status.subscribe(x=>{
-        console.log("test",x);
-      })
-
+    this._connection = this.signalR.createConnection(config);
+    return Observable.fromPromise(this._connection.start()).switchMap(connection => {
       this._connection.errors.subscribe(errors => {
         this._connection.stop();
-        console.log(errors);
       });
 
       return Observable.of(connection);
@@ -124,9 +107,7 @@ export class SignalrServiceReal extends SignalrService {
           this.notificationConnection.stop();
           console.log(errors);
         })
-      }).catch(error => {
-        console.log(error);
-      });
+      }).catch(error => console.log(error));
 
     } else {
       if (this.notificationConnection) this.notificationConnection.stop();
